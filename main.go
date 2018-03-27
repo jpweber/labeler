52c23e54651649403690929eb808381bbb09443e
Jim Weber <jpweber@gmail.com>
2018-03-27 16:59:34 -0400

    Some refactoring. Took out the auto config reload for now.

--- main.go
+++ main.go
@@ -2,75 +2,24 @@ package main
 
 import (
 	"flag"
-	"log"
 	"os"
 	"path/filepath"
-	"time"
 
+	"github.com/jpweber/labeler/config"
 	"github.com/jpweber/labeler/node"
-	"k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func Nodes(client *kubernetes.Clientset, excludes map[string]bool) {
-
-	watchlist := cache.NewListWatchFromClient(client.Core().RESTClient(), "nodes", v1.NamespaceAll,
-		fields.Everything())
-	_, controller := cache.NewInformer(
-		watchlist,
-		&v1.Node{},
-		time.Second*600,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				log.Println("Running Add Function")
-				K8sNode := obj.(*v1.Node)
-				node := node.Node{
-					Name:       K8sNode.ObjectMeta.Name,
-					ExternalID: K8sNode.Spec.ExternalID,
-					Excludes:   excludes,
-				}
-
-				go func(K8sNode *v1.Node) {
-					// add the tags on to the node struct
-					node.ListTags()
-					origTagLen := len(K8sNode.GetLabels())
-					// apply the tags to labels on the k8s node
-					K8sNode = node.AddLabels(K8sNode)
-					if origTagLen == len(K8sNode.GetLabels()) {
-						log.Println("Not Updating", node.Name, "No changes to apply")
-					} else {
-						log.Println("Applying new labels to", node.Name)
-						// update the actual node
-						_, err := client.CoreV1().Nodes().Update(K8sNode)
-						if err != nil {
-							log.Println("Error updating node", err)
-						}
-					}
-				}(K8sNode)
-
-			},
-		},
-	)
-	stop := make(chan struct{})
-	done := make(chan bool)
-	go controller.Run(stop)
-	log.Println("Started Watching Nodes")
-	<-done
-}
 func homeDir() string {
 	return os.Getenv("HOME")
 }
 
 func main() {
 
-	exludes := map[string]bool{
-		"Name": true,
-		"aws:autoscaling:groupName": true,
-		"kubernetes.io/cluster/jpw": true,
-	}
+	configPath := "config.yaml"
+	appConfig := config.ReadConfig(configPath)
+
 	var kubeconfig *string
 	// var namespace *string
 	if home := homeDir(); home != "" {
@@ -79,7 +28,8 @@ func main() {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 
-	// namespace = flag.String("namespace", "default", "name space to query")
+	// ns := flag.String("namespace", "default", "name space to query")
+	// not used yet
 	flag.Parse()
 
 	// use the current context in kubeconfig
@@ -94,6 +44,7 @@ func main() {
 		panic(err.Error())
 	}
 
-	// Pods(clientset)
-	Nodes(clientset, exludes)
+	// Start node watcher
+	n := node.Node{}
+	n.Watcher(clientset, appConfig.Excludes)
 }
--- node/node.go
+++ node/node.go
@@ -1,8 +1,16 @@
 package node
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/jpweber/labeler/provider"
 	"k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 )
 
 type Node struct {
@@ -12,11 +20,59 @@ type Node struct {
 	Excludes   map[string]bool
 }
 
-func (n *Node) ListTags() {
-	n.Tags = provider.EC2Tags(n.ExternalID)
+// Watcher - starts the watcher for nodes joining the cluster
+// and triggers the adding of labels on new node connection
+func (n *Node) Watcher(client *kubernetes.Clientset, excludes map[string]bool) {
+
+	watchlist := cache.NewListWatchFromClient(client.Core().RESTClient(), "nodes", v1.NamespaceAll,
+		fields.Everything())
+	_, controller := cache.NewInformer(
+		watchlist,
+		&v1.Node{},
+		time.Second*600,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				log.Println("Running Add Function")
+				K8sNode := obj.(*v1.Node)
+
+				n.Name = K8sNode.ObjectMeta.Name
+				n.ExternalID = K8sNode.Spec.ExternalID
+				n.Excludes = excludes
+
+				go func(K8sNode *v1.Node) {
+					// add the tags on to the node struct
+					n.ProviderTags()
+
+					// apply the tags to labels on the k8s node
+					K8sNode = n.GenNewLabelSet(K8sNode)
+					// log.Println("Applying new labels to", n.Name)
+					// // update the actual node
+					// _, err := client.CoreV1().Nodes().Update(K8sNode)
+					// if err != nil {
+					// 	log.Println("Error updating node", err)
+					// 	updatedNode, _ := client.CoreV1().Nodes().Get(K8sNode.Name, meta_v1.GetOptions{})
+					// 	log.Println(updatedNode)
+					// }
+					ApplyLabels(client, K8sNode)
+				}(K8sNode)
+
+			},
+		},
+	)
+	stop := make(chan struct{})
+	done := make(chan bool)
+	go controller.Run(stop)
+	log.Println("Started Watching Nodes")
+	<-done
 }
 
-func (n *Node) AddLabels(K8sNode *v1.Node) *v1.Node {
+func (n *Node) ProviderTags() {
+	n.Tags = provider.EC2Tags(n.ExternalID)
+	// as we add different cloud providrers
+	// create a swtich statement to fetch from different sources
+}
+
+func (n *Node) GenNewLabelSet(K8sNode *v1.Node) *v1.Node {
 	newlabels := make(map[string]string)
 
 	// filter out any explicitly ecluded labels
@@ -25,17 +81,28 @@ func (n *Node) AddLabels(K8sNode *v1.Node) *v1.Node {
 			newlabels[k] = v
 		}
 	}
+
+	// fetch existing node labels
 	labels := K8sNode.GetLabels()
+	objMeta := K8sNode.GetResourceVersion()
+	fmt.Println(objMeta)
 	// add the new labels
 	for k, v := range newlabels {
 		labels[k] = v
 	}
-	// fmt.Println("old")
-	// fmt.Printf("%+v", K8sNode.GetLabels())
+
 	K8sNode.SetLabels(labels)
-	// fmt.Println("here be labels")
-	// fmt.Println("new")
-	// fmt.Printf("%+v", K8sNode.GetLabels())
 
 	return K8sNode
+}
+
+func ApplyLabels(client *kubernetes.Clientset, K8sNode *v1.Node) {
+	log.Println("Applying new labels to", K8sNode.ObjectMeta.Name)
+	// update the actual node
+	_, err := client.CoreV1().Nodes().Update(K8sNode)
+	if err != nil {
+		log.Println("Error updating node", err)
+		updatedNode, _ := client.CoreV1().Nodes().Get(K8sNode.Name, meta_v1.GetOptions{})
+		ApplyLabels(client, updatedNode)
+	}
 }
